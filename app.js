dfaa9342fe6a9ff3d01fd417fe35ae4475920b2b
rita-vrataski <elayneman@hotmail.com>
2016-10-19 23:09:07 -1000

    ok

--- app.js
+++ app.js
@@ -285,6 +285,10 @@ io.sockets.on('connection', function (socket) {
     //socket.emit('init-results', { drivers: data.drivers, lastpoll: data.poller.lastpoll.formatDate('hh:mm:ss t'), runcount: data.runs.length });
 
     socket.on('join-runs', function (d) {
+		
+		//ADDED THIS
+		socket.emit('init-results', { drivers: data.drivers, lastpoll: data.poller.lastpoll.formatDate('hh:mm:ss t'), runcount: data.runs.length });
+		
         socket.join('runs');
         var last20 = [], runCount = data.runs.length;
 
