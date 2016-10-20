970ae90186a0580a04c444d6f9927d45c2bbf671
rita-vrataski <elayneman@hotmail.com>
2016-10-19 21:23:06 -1000

    ok

--- jquery/incremental.js
+++ jquery/incremental.js
@@ -112,7 +112,8 @@ function genTtod() {
         var c = ttod[i];
         html += '<li data-role="list-divider" class="ui-li-heading">' + c.category + '</li>';
         //html += '<li><a href="/drivertimes?n=' + c.car.number + '"><div style="position:absolute;font-size:50px;opacity:.3;right:20%;font-style:italic;">' + c.car.number + '</div><span class="ui-li-aside">' + c.value + '</span><h3 class="ui-li-heading">' + c.driver + '</h3><p>' + c.car.description + ' </p></a></li>';
-        html += '<li><a id="ttod-' + i + '"><div style="position:absolute;font-size:50px;opacity:.3;right:20%;font-style:italic;">' +  c.value + '</div><span class="ui-li-aside"></span><h3 class="ui-li-heading">' + c.driver + ' <font style="font-weight:normal;">' + c.axclass + ' ' +c.car.number + '</font></h3><p>' + c.car.description + ' </p></a></li>';
+		//html += '<li><a id="ttod-' + i + '"><div style="position:absolute;font-size:50px;opacity:.3;right:20%;font-style:italic;">' +  c.value + '</div><span class="ui-li-aside"></span><h3 class="ui-li-heading">' + c.driver + ' <font style="font-weight:normal;">' + c.axclass + ' ' +c.car.number + '</font></h3><p>' + c.car.description + ' </p></a></li>';
+        html += '<li><a id="ttod-' + i + '"><div style="position:absolute;font-size:50px;opacity:.3;right:20%;font-style:italic;">' +  c.value + '</div><h3 class="ui-li-heading">' + c.driver + ' <font style="font-weight:normal;">' + c.axclass + ' ' +c.car.number + '</font></h3><p>' + c.car.description + ' </p></a></li>';
 		
     }
     $('#ttodresults').html(html).listview('refresh').find('a').click(function () {
