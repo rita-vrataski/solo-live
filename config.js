3bf356c8950c1bbd2528882e6ac2541e62c4dc2c
SCCA Hawaii <SCCA Hawaii>
2017-12-10 08:19:31 -1000

    Solo

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-﻿var config = {}; config.datafile = "Data.st1"; config.port = 5000; config.useTod = true; config.maxRunsCounted = 6; config.useSuperClassing = true; config.allowFunInOverall = false; config.secondsPerCone = 2; module.exports = config;
+﻿var config = {}; config.datafile = "Data.st1"; config.port = 5000; config.useTod = true; config.maxRunsCounted = 10; config.useSuperClassing = true; config.allowFunInOverall = false; config.secondsPerCone = 2; module.exports = config;
 
